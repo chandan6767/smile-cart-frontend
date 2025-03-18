fa100588c2efa032e132c90a045ef7c01d6d3377
chankruze <chankruze@gmail.com>
2025-03-18 15:40:23 +0530

    Replaced CartItemsContext with useCartItemsStore
    - changed item card width to fit remove from cart button
    - change the button background color of add to cart button
    
    Signed-off-by: chankruze <chankruze@gmail.com>

--- src/components/ProductList/ProductListItem.jsx
+++ src/components/ProductList/ProductListItem.jsx
@@ -6,7 +6,7 @@ import { buildUrl } from "utils/url";
 
 const ProductListItem = ({ imageUrl, name, offerPrice, slug }) => (
   <Link
-    className="neeto-ui-border-black neeto-ui-rounded-xl flex w-48 flex-col items-center justify-between border p-4"
+    className="neeto-ui-border-black neeto-ui-rounded-xl flex w-52 flex-col items-center justify-between border p-4"
     to={buildUrl(routes.products.show, { slug })}
   >
     <img alt={name} className="h-40 w-40" src={imageUrl} />
--- src/components/commons/AddToCart.jsx
+++ src/components/commons/AddToCart.jsx
@@ -1,26 +1,30 @@
-import { useContext } from "react";
-
+import classNames from "classnames";
 import { Button } from "neetoui";
-import { without } from "ramda";
-import CartItemsContext from "src/contexts/CartItemsContext";
+import useCartItemsStore from "stores/useCartItemsStore";
+import { shallow } from "zustand/shallow";
 
 const AddToCart = ({ slug }) => {
-  const [cartItems, setCartItems] = useContext(CartItemsContext);
+  const { isInCart, toggleIsInCart } = useCartItemsStore(
+    store => ({
+      isInCart: store.cartItems.includes(slug),
+      toggleIsInCart: store.toggleIsInCart,
+    }),
+    shallow
+  );
 
   const handleClick = e => {
     e.stopPropagation();
     e.preventDefault();
-    setCartItems(prevCartItems =>
-      prevCartItems.includes(slug)
-        ? without([slug], cartItems)
-        : [slug, ...cartItems]
-    );
+    toggleIsInCart(slug);
   };
 
   return (
     <Button
-      label={cartItems.includes(slug) ? "Remove from cart" : "Add to cart"}
+      label={isInCart ? "Remove from cart" : "Add to cart"}
       size="large"
+      className={classNames({
+        "bg-red-600 hover:bg-red-500": isInCart,
+      })}
       onClick={handleClick}
     />
   );
