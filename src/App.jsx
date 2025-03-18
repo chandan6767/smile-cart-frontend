231fb7d041fcf56abfce6c567bee9107b4084660
chankruze <chankruze@gmail.com>
2025-03-18 15:08:56 +0530

    Added add to cart feature on the product details page
    Signed-off-by: chankruze <chankruze@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { PageNotFound } from "components/commons";
 import Product from "components/Product";
 import ProductList from "components/ProductList";
@@ -5,14 +7,21 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import routes from "routes";
 
 import "./App.css";
+import CartItemsContext from "./contexts/CartItemsContext";
 
-const App = () => (
-  <Switch>
-    <Route exact component={ProductList} path={routes.products.index} />
-    <Route exact component={Product} path={routes.products.show} />
-    <Redirect exact from="/" to={routes.products.index} />
-    <Route component={PageNotFound} path="*" />
-  </Switch>
-);
+const App = () => {
+  const [cartItems, setCartItems] = useState([]);
+
+  return (
+    <CartItemsContext.Provider value={[cartItems, setCartItems]}>
+      <Switch>
+        <Route exact component={ProductList} path={routes.products.index} />
+        <Route exact component={Product} path={routes.products.show} />
+        <Redirect exact from="/" to={routes.products.index} />
+        <Route component={PageNotFound} path="*" />
+      </Switch>
+    </CartItemsContext.Provider>
+  );
+};
 
 export default App;
