2f0eff5fb497e8f253165d26837ffdae71ccca34
chankruze <chankruze@gmail.com>
2025-03-19 16:57:39 +0530

    Added pagination to product listing page
    Signed-off-by: chankruze <chankruze@gmail.com>

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -4,18 +4,25 @@ import { Header, PageLoader } from "components/commons";
 import { useFetchProducts } from "hooks/reactQuery/useProductsApi";
 import useDebounce from "hooks/useDebounce";
 import { Search } from "neetoicons";
-import { Input, NoData } from "neetoui";
+import { Input, NoData, Pagination } from "neetoui";
 import { isEmpty, without } from "ramda";
 
+import { DEFAULT_PAGE_INDEX, DEFAULT_PAGE_SIZE } from "./constants";
 import ProductListItem from "./ProductListItem";
 
 const ProductList = () => {
   const [cartItems, setCartItems] = useState([]);
   const [searchKey, setSearchKey] = useState("");
   const debouncedSearchKey = useDebounce(searchKey);
-  const { data: { products = [] } = {}, isLoading } = useFetchProducts({
+  const [currentPage, setCurrentPage] = useState(DEFAULT_PAGE_INDEX);
+  const productsParams = {
     searchTerm: debouncedSearchKey,
-  });
+    page: currentPage,
+    pageSize: DEFAULT_PAGE_SIZE,
+  };
+
+  const { data: { products = [], totalProductsCount } = {}, isLoading } =
+    useFetchProducts(productsParams);
 
   const toggleIsInCart = slug =>
     setCartItems(prevCartItems =>
@@ -38,23 +45,36 @@ const ProductList = () => {
             prefix={<Search />}
             type="search"
             value={searchKey}
-            onChange={e => setSearchKey(e.target.value)}
+            onChange={e => {
+              setSearchKey(e.target.value);
+              setCurrentPage(DEFAULT_PAGE_INDEX);
+            }}
           />
         }
       />
       {isEmpty(products) ? (
         <NoData className="h-full w-full" title="No products to show" />
       ) : (
-        <div className="grid grid-cols-2 justify-items-center gap-y-8 p-4 md:grid-cols-3 lg:grid-cols-4">
-          {products.map(product => (
-            <ProductListItem
-              key={product.slug}
-              {...product}
-              isInCart={cartItems.includes(product.slug)}
-              toggleIsInCart={() => toggleIsInCart(product.slug)}
+        <>
+          <div className="grid grid-cols-2 justify-items-center gap-y-8 p-4 md:grid-cols-3 lg:grid-cols-4">
+            {products.map(product => (
+              <ProductListItem
+                key={product.slug}
+                {...product}
+                isInCart={cartItems.includes(product.slug)}
+                toggleIsInCart={() => toggleIsInCart(product.slug)}
+              />
+            ))}
+          </div>
+          <div className="self-end p-6">
+            <Pagination
+              count={totalProductsCount}
+              navigate={page => setCurrentPage(page)}
+              pageNo={currentPage || DEFAULT_PAGE_INDEX}
+              pageSize={DEFAULT_PAGE_SIZE}
             />
-          ))}
-        </div>
+          </div>
+        </>
       )}
     </div>
   );
